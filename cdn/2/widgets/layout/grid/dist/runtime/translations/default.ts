0c0c88f24925f37cd4351b39ea04b0db7d61e119
Shoaib Akhtar <36438978+s4shebi@users.noreply.github.com>
2025-11-01 03:19:17 +0500

    updated widgets

--- cdn/2/widgets/layout/grid/dist/runtime/translations/default.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  _widgetLabel: 'Grid',
-  widgetProperties: 'Widget properties',
-  widgetFunctions: 'Widget functions',
-  widgetName: 'widget name:',
-  widgetProps: 'widget properties:',
-  tips: 'Grid'
-}
